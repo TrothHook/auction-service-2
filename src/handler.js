6c0cdff77857eba7692a056aea81c51064339e28
TrothHook <HrithikSarkar45@protonmail.com>
2025-07-13 00:05:11 +0530

    Initial Commit

---
+++ src/handler.js
@@ -0,0 +1,27 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+export const createAuction = async (event, context) => {
+  const { title } = JSON.parse(event.body);
+
+  const auction = {
+    id: uuid(),
+    title,
+    status: "OPEN",
+    createdAt: new Date().toLocaleString("sv-SE"),
+  };
+
+  await dynamodb
+    .put({
+      TableName: "AuctionsTable",
+      Item: auction,
+    })
+    .promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+};
